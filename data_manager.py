48379d615affba08d545c7187e47c2a8f167ca57
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-10-03 15:03:31 +0200

    Implement data manager with CRUD operations for User and Movie models

---
+++ data_manager.py
@@ -0,0 +1,40 @@
+from models import db, User, Movie
+
+class DataManager():
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def get_users(self):
+        return User.query.all()
+
+    def get_movies(self, user_id):
+        return Movie.query.filter_by(user_id=user_id).all()
+
+    def add_movie(self, movie):
+        try:
+            db.session.add(movie)
+            db.session.commit()
+            return True
+        except:
+            db.session.rollback()
+            return False
+
+    def update_movie(self, movie_id, new_title):
+        movie_to_update = Movie.query.filter_by(id=movie_id).first()
+        if movie_to_update:
+            movie_to_update.name = new_title
+            db.session.commit()
+            return True
+        return False
+
+    def delete_movie(self, movie_id):
+        movie_to_delete = Movie.query.filter_by(id=movie_id).first()
+        if movie_to_delete:
+            db.session.delete(movie_to_delete)
+            db.session.commit()
+            return True
+        return False
+
+
