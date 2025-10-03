aa092904868a98fad4c3c431c95db82a7287d9bb
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-10-03 13:59:38 +0200

    Define SQLAlchemy models for User and Movie including relationships

--- models.py
+++ models.py
@@ -1,3 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    movies = db.relationship('Movie', backref='user', lazy='True')
+
+    def __str__(self):
+        return f"Username: {self.name} - ID: {self.id}"
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    poster_url = db.Column(db.String, nullable=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __str__(self):
+        return f"Movie: {self.name} - ID: {self.id}, Year: {self.year}, director: {self.director}, Poster URL: {self.poster_url}"
 
